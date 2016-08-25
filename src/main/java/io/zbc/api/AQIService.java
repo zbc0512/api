f0d890d04fc88d661f6a11c8952e63a892ddd5a2
ZhangBaicheng <zbc0512@163.com>
2016-08-25 17:01:25 +0800

    无用的import

--- src/main/java/io/zbc/api/AQIService.java
+++ src/main/java/io/zbc/api/AQIService.java
@@ -1,11 +1,6 @@
 package io.zbc.api;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
